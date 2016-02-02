3e246b4a226112f4329c647b35ee2d919b393378
bychkov <bychkov@raa-st.com>
2016-02-02 19:01:58 +0300

    3

--- chann3.cpp
+++ chann3.cpp
@@ -10,30 +10,20 @@ int main(int argc, char** argv)
     const unsigned int buff_size = 32;
     char buffer[buff_size];
 
-    socketpair(PF_LOCAL, SOCK_STREAM, 0, fd);
+    socketpair(AF_UNIX, SOCK_STREAM, 0, fd);
     printf("Crated sockets pair [%d-%d]\n", fd[0], fd[1]);
 
     if(fork())
     {
         // parent
-        close(fd[1]);
-        fgets(buffer, buff_size, stdin);
+        read(fd[1], buffer, buff_size);
         write(fd[0], buffer, buff_size);
     }
     else
     {
         // child
-        unsigned int len;
-
-        memset(buffer, 0, buff_size);
-
-        close(fd[0]);
-        len = read(fd[1], buffer, buff_size);
-        if(len <= 0)
-        {
-            printf("Read error\n");
-        }
-        printf("%s\n", buffer);
+        read(fd[0], buffer, buff_size);
+        write(fd[1], buffer, buff_size);
     }
 
     return 0;
