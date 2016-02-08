108917d6da3411f64970cacac438cc8ee46fc217
DPOH357 <DPOH357@yandex.ru>
2016-02-08 16:47:14 +0000

    111

--- shm.cpp
+++ shm.cpp
@@ -6,11 +6,11 @@ int main(int argc, char** argv)
 {
 	key_t key = ftok("/tmp/mem.temp", 1);
 
-	int id = shmget(key, 1024, IPC_CREAT | IPC_EXCL | 0660);
+	int id = shmget(key, 1048576, IPC_CREAT | 0660);
 
 	char* ptr = (char*) shmat(id, 0, 0);
 
-	for(int i = 0; i < 1024; i++)
+	for(int i = 0; i < 1048576; i++)
 	{
 		ptr[i] = 42;
 	}
