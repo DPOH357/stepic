a31e4b610aa2210105a8c2b830d598c69d4d878a
DPOH357 <DPOH357@uandex.ru>
2016-02-08 16:30:49 +0000

    9

--- shm2.cpp
+++ shm2.cpp
@@ -12,16 +12,18 @@ int main(int argc, char** argv)
 
 	fd = shm_open("/test.shm", O_CREAT | O_RDWR, 0660);
 
-	ftruncate(fd, 1024);
+	ftruncate(fd, 1048576);
 
-	ptr = (char*) mmap(0, 1024, PROT_WRITE, MAP_SHARED, fd, 0);
+	ptr = (char*) mmap(0, 1048576, PROT_WRITE | PROT_READ, MAP_SHARED, fd, 0);
 
-	for(i = 0; i < 1024; i++)
+	for(i = 0; i < 1048576; i++)
 	{
 		ptr[i] = 13;
 	}
 
-	munmap(ptr, 1024);
+	//munmap(ptr, 1024);
+
+	//shm_unlink("/test.shm");
 
 	return 0;
 }
